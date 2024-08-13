eb7bc35ad71fdcce96abd3b120e3e1484707771f
amitai <amitiy36@gmail.com>
2024-08-13 17:11:17 +0300

    create a generic dialog around stepper

---
+++ client/src/components/DialogMui.tsx
@@ -0,0 +1,49 @@
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useTheme } from "@mui/material/styles";
+import { Close } from "@mui/icons-material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import * as React from "react";
+
+interface DialogComponentProps {
+  open: boolean;
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  content: React.ReactNode;
+  title: { text: string; color: string };
+  whenClose?: () => void;
+}
+
+export default function DialogComponent(props: DialogComponentProps) {
+  const {
+    content,
+    open,
+    setOpen,
+    title: { text, color },
+    whenClose,
+  } = props;
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleClose = () => {
+    setOpen?.(false);
+    whenClose?.();
+  };
+
+  return (
+    <Dialog fullWidth fullScreen={fullScreen} open={open} onClose={handleClose}>
+      <DialogActions>
+        <Button autoFocus onClick={handleClose}>
+          <Close />
+        </Button>
+      </DialogActions>
+      <DialogTitle color={color}>{text}</DialogTitle>
+      <DialogContent>
+        {content}
+      </DialogContent>
+    </Dialog>
+  );
+}
--- client/src/pages/StepperJson.tsx
+++ client/src/pages/StepperJson.tsx
@@ -1,9 +1,13 @@
+import DialogComponent from "../components/DialogMui"
 import StepperMui from "../components/StepperMui"
 
 function StepperJson() {
     return (
         <div>
-            <StepperMui components={[<>h1</>, <h2>h2</h2>]} steps={["hi", "a", "b", "c"]} />
+            <DialogComponent content={
+                <StepperMui components={[<h1>h1</h1>, <h2>h2</h2>]} steps={["hi", "a", "b", "c"]} />
+            } open={true} title={{ text: "json configuretion file", color: "info" }}  >
+            </DialogComponent>
         </div>
     )
 }
