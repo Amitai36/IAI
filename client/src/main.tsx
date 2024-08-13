375e38303f68462e2093df26f9d4a551cb971518
amitai <amitiy36@gmail.com>
2024-08-14 01:32:18 +0300

    create select in branch selection

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
+import { QueryClientProvider, QueryClient } from "react-query"
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react'
 
+import App from './App.tsx'
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 )
