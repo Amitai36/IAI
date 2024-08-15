0545b2caee3747686332260233a50966a1347c27
amitai <amitiy36@gmail.com>
2024-08-15 23:47:09 +0300

    create array option to fill with validate and err

--- client/src/pages/RecentFixes.tsx
+++ client/src/pages/RecentFixes.tsx
@@ -1,37 +1,73 @@
-import { Button, Grid, IconButton, TextField } from "@mui/material"
 import { Add, Delete } from "@mui/icons-material"
-import { useState } from "react"
+import { useForm, useFieldArray, Controller, SubmitHandler } from "react-hook-form"
+import { Button, Grid, IconButton, Stack, TextField, Typography } from "@mui/material"
 
+
+interface FixesProps { fixes: { fix: string }[] }
 
 function RecentFixes() {
 
-    const [fixes, setFixes] = useState<string[]>([''])
-    const onDelete = (key: number) => {
-        setFixes(prevItems =>
-            prevItems.slice(0, key).concat(prevItems.slice(key + 1))
-        )
-    }
-    const onAdd = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, key: number) => {
-        const currentValue = e.target.value
-        const copy = [...fixes]
-        copy[key] = currentValue
-        setFixes(copy)
+    const { control, handleSubmit, formState: { errors } }
+        = useForm<FixesProps>({
+            defaultValues: {
+                fixes: [{ fix: "" }]
+            }
+        });
+    const { fields, append, remove } = useFieldArray<{ fixes: { fix: string }[] }>({
+        control,
+        name: "fixes"
+    });
+
+    const onSubmit: SubmitHandler<FixesProps> = (event) => {
+        console.log(event)
     }
 
     return (
-        <div style={{ alignItems: 'center', }}>
-            <IconButton onClick={() => setFixes(prev => [...prev, ''])}><Add /></IconButton>
-            <Grid container spacing={2}>
-                {fixes.map((item, key) =>
-                    <Grid item xs={3}>
-                        <TextField InputProps={{
-                            endAdornment: (
-                                <Button onClick={() => onDelete(key)}><Delete />
-                                </Button>)
-                        }} key={key} value={item} onChange={(e) => onAdd(e, key)} />
-                    </Grid>)}
-            </Grid>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={2}>
+                <div style={{ width: "10%" }}>
+                    <IconButton
+                        onClick={() => append({ fix: "" })}>
+                        <Add />
+                    </IconButton>
+                </div>
+                <Grid container columnSpacing={1} rowSpacing={2}>
+                    {fields.map((item, index) => (
+                        <Grid item xs={6} key={item.id}>
+                            <Controller
+                                rules={{ required: "This field can't be empty", }}
+                                name={`fixes.${index}.fix`}
+                                control={control}
+                                render={({ field }) => (
+                                    <>
+                                        <TextField
+                                            {...field}
+                                            label="Fix"
+                                            error={!!errors.fixes?.[index]}
+                                            InputProps={{
+                                                endAdornment: (
+                                                    <Button onClick={() => remove(index)}><Delete /></Button>
+                                                )
+                                            }}
+                                        />
+                                        <Typography component={"span"} >
+                                            {errors.fixes?.[index]?.fix?.message}
+                                        </Typography>
+                                    </>
+                                )}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+                <div style={{ width: "10%" }}>
+                    <Button type="submit"
+                        color='success'
+                        variant='contained'
+                    >save/edit
+                    </Button>
+                </div>
+            </Stack>
+        </form >
     )
 }
 
